f48576528bac4d53d6da56cdcc7b87862bd9809c
Pedro Martin Valera <pataruco@gmail.com>
2025-11-01 17:37:03 +0000

    choe: abstract queries

--- apps/server/src/main.rs
+++ apps/server/src/main.rs
@@ -6,26 +6,17 @@ use axum::{
     routing::get,
 };
 
+mod api;
+use api::Query;
+
 #[tokio::main]
 async fn main() {
     let schema = Schema::build(Query, EmptyMutation, EmptySubscription).finish();
-
     let router = Router::new().route("/graphql", get(graphql).post_service(GraphQL::new(schema)));
-
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-
     axum::serve(listener, router).await.unwrap();
 }
 
 async fn graphql() -> impl IntoResponse {
     Html(GraphiQLSource::build().endpoint("/graphql").finish())
-}
-
-struct Query;
-
-#[Object]
-impl Query {
-    async fn hello(&self) -> String {
-        "hello world".to_string()
-    }
 }
