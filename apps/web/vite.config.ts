f9e921bba0c4fe33b6cc5aa6214d05f04d0e7576
Pedro Martin <pataruco@gmail.com>
2026-02-20 14:04:22 +0000

    feat: add redux

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,11 +1,7 @@
-/// <reference types="vitest" />
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	test: {
-		globals: true,
-	},
+  plugins: [react()],
 });
