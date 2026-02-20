acfdeddf9259453b11e8a904c50f1916a41a8581
Pedro Martin <pataruco@gmail.com>
2026-02-20 20:14:27 +0000

    chore: save generated next to graphql document

--- apps/web/config/codegen.ts
+++ apps/web/config/codegen.ts
@@ -1,48 +1,29 @@
 import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const GUACAL_GRAPHQL_API =
-  process.env.VITE_GOOGLE_MAPS_API_KEYGUACAL_GRAPHQL_API ??
-  'http://0.0.0.0:8080/graphql';
+  process.env.VITE_GUACAL_GRAPHQL_API ?? 'http://0.0.0.0:8080/graphql';
 
-// const config: CodegenConfig = {
-// 	documents: ["website/src/graphql/**/*.graphql"],
-// 	generates: {
-// 		"website/src/graphql/": {
-// 			config: {
-// 				withResultType: true,
-// 			},
-// 			overwrite: true,
-// 			plugins: ["typescript-operations", "typed-document-node"],
-// 			preset: "near-operation-file",
-// 			presetConfig: {
-// 				baseTypesPath: "types.ts",
-// 				extension: ".generated.ts",
-// 			},
-// 		},
-// 		"website/src/graphql/types.ts": {
-// 			overwrite: true,
-// 			plugins: ["typescript"],
-// 		},
-// 	},
-// 	schema: {
-// 		[GUACAL_GRAPHQL_API]: {},
-// 	},
-// };
-//
 const config: CodegenConfig = {
   documents: './src/**/*.graphql',
   generates: {
-    './src/graphql/generated.ts': {
+    './src/': {
       plugins: [
-        'typescript',
         'typescript-operations',
         {
           'typescript-rtk-query': {
             exportHooks: true,
-            importBaseApiFrom: '../store/features/guacal-api/base',
+            importBaseApiFrom: '@/store/features/guacal-api/base',
           },
         },
       ],
+      preset: 'near-operation-file',
+      presetConfig: {
+        baseTypesPath: 'graphql/types.ts',
+        extension: '.generated.ts',
+      },
+    },
+    './src/graphql/types.ts': {
+      plugins: ['typescript'],
     },
   },
   schema: GUACAL_GRAPHQL_API,
