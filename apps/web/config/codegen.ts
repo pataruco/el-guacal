f32e60475e847d4b9d29ef2517304285670a11cc
Pedro Martin <pataruco@gmail.com>
2026-02-20 21:05:31 +0000

    feat: getting data from guacal server

--- apps/web/config/codegen.ts
+++ apps/web/config/codegen.ts
@@ -1,33 +1,33 @@
-import type { CodegenConfig } from "@graphql-codegen/cli";
+import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const GUACAL_GRAPHQL_API =
-	process.env.VITE_GUACAL_GRAPHQL_API ?? "http://0.0.0.0:8080/graphql";
+  process.env.VITE_GUACAL_GRAPHQL_API ?? 'http://0.0.0.0:8080/graphql';
 
 const config: CodegenConfig = {
-	documents: "./src/**/*.graphql",
-	generates: {
-		"./src/": {
-			plugins: [
-				"typescript-operations",
-				{
-					"typescript-rtk-query": {
-						exportHooks: true,
-						importBaseApiFrom: "@/store/features/guacal-api/base",
-						importBaseApiAlternateName: "gualcalGraphqlApiSlice",
-					},
-				},
-			],
-			preset: "near-operation-file",
-			presetConfig: {
-				baseTypesPath: "graphql/types.ts",
-				extension: ".generated.ts",
-			},
-		},
-		"./src/graphql/types.ts": {
-			plugins: ["typescript"],
-		},
-	},
-	schema: GUACAL_GRAPHQL_API,
+  documents: './src/**/*.graphql',
+  generates: {
+    './src/': {
+      plugins: [
+        'typescript-operations',
+        {
+          'typescript-rtk-query': {
+            exportHooks: true,
+            importBaseApiAlternateName: 'gualcalGraphqlApiSlice',
+            importBaseApiFrom: '@/store/features/guacal-api/base',
+          },
+        },
+      ],
+      preset: 'near-operation-file',
+      presetConfig: {
+        baseTypesPath: 'graphql/types.ts',
+        extension: '.generated.ts',
+      },
+    },
+    './src/graphql/types.ts': {
+      plugins: ['typescript'],
+    },
+  },
+  schema: GUACAL_GRAPHQL_API,
 };
 
 export default config;
--- apps/web/src/components/map/index.tsx
+++ apps/web/src/components/map/index.tsx
@@ -1,53 +1,54 @@
 import {
-	APIProvider,
-	Map as GoogleMap,
-	type MapCameraChangedEvent,
-} from "@vis.gl/react-google-maps";
-import { useGetStoresNearQuery } from "@/graphql/queries/get-stores-near/index.generated";
-import { Radius } from "../../graphql/types";
+  APIProvider,
+  Map as GoogleMap,
+  type MapCameraChangedEvent,
+} from '@vis.gl/react-google-maps';
+import { useGetStoresNearQuery } from '@/graphql/queries/get-stores-near/index.generated';
+import { Radius } from '../../graphql/types';
 import {
-	getUserLocation,
-	selectMap,
-	setCenter,
-	setZoom,
-} from "../../store/features/map/slice";
-import { useAppDispatch, useAppSelector } from "../../store/hooks";
-import styles from "./index.module.scss";
+  getUserLocation,
+  selectMap,
+  setCenter,
+  setZoom,
+} from '../../store/features/map/slice';
+import { useAppDispatch, useAppSelector } from '../../store/hooks';
+import styles from './index.module.scss';
 
 const GOOGLE_MAPS_API_KEY = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
 
 const MapComponent = () => {
-	const dispatch = useAppDispatch();
-	const { center } = useAppSelector(selectMap);
+  const dispatch = useAppDispatch();
+  const { center } = useAppSelector(selectMap);
 
-	const { data, isLoading } = useGetStoresNearQuery({
-		location: center,
-		radius: Radius.Km_10,
-	});
+  const { data, isLoading, error } = useGetStoresNearQuery({
+    location: center,
+    radius: Radius.Km_10,
+  });
 
-	console.log({ data, isLoading });
+  // console.log({ data, error, isLoading });
 
-	const handleOnLoad = () => {
-		dispatch(getUserLocation());
-	};
+  const handleOnLoad = () => {
+    dispatch(getUserLocation());
+  };
 
-	const handleOnCameraChanged = (ev: MapCameraChangedEvent) => {
-		dispatch(setCenter(ev.detail.center));
-		dispatch(setZoom(ev.detail.zoom));
-	};
+  const handleOnCameraChanged = (ev: MapCameraChangedEvent) => {
+    console.log(ev.detail.zoom);
+    dispatch(setCenter(ev.detail.center));
+    dispatch(setZoom(ev.detail.zoom));
+  };
 
-	return (
-		<div className={styles.map}>
-			<APIProvider apiKey={GOOGLE_MAPS_API_KEY} onLoad={handleOnLoad}>
-				<GoogleMap
-					defaultZoom={13}
-					center={center}
-					defaultCenter={center}
-					onCameraChanged={handleOnCameraChanged}
-				/>
-			</APIProvider>
-		</div>
-	);
+  return (
+    <div className={styles.map}>
+      <APIProvider apiKey={GOOGLE_MAPS_API_KEY} onLoad={handleOnLoad}>
+        <GoogleMap
+          defaultZoom={13}
+          center={center}
+          defaultCenter={center}
+          onCameraChanged={handleOnCameraChanged}
+        />
+      </APIProvider>
+    </div>
+  );
 };
 
 export default MapComponent;
