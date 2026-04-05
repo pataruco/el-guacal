586da2f916e4e86ae7bb8455574f1ca797177fea
Pedro Martin <pataruco@gmail.com>
2026-04-05 20:17:58 +0100

    fix: gtm

--- apps/web/app/components/cookie-banner/index.tsx
+++ apps/web/app/components/cookie-banner/index.tsx
@@ -3,12 +3,18 @@ import { useLocalStorage } from '@/hooks/use-local-storage';
 import {
   LOCAL_STORAGE_KEY_TRACKING_KEY,
   TRACKING_CONSENT,
+  updateConsent,
 } from '@/utils/analytics';
 import styles from './index.module.scss';
 
 const CookieBanner = () => {
   const [isHydrated, setIsHydrated] = useState(false);
   const [consent, setConsent] = useLocalStorage(LOCAL_STORAGE_KEY_TRACKING_KEY);
+
+  const handleConsent = (value: TRACKING_CONSENT) => {
+    setConsent(value);
+    updateConsent(value);
+  };
 
   useEffect(() => {
     setIsHydrated(true);
@@ -40,14 +46,14 @@ const CookieBanner = () => {
           <button
             type="button"
             className={`${styles['c-cookie-banner__btn']} ${styles['c-cookie-banner__btn--accept']}`}
-            onClick={() => setConsent(TRACKING_CONSENT.GRANTED)}
+            onClick={() => handleConsent(TRACKING_CONSENT.GRANTED)}
           >
             Accept
           </button>
           <button
             type="button"
             className={`${styles['c-cookie-banner__btn']} ${styles['c-cookie-banner__btn--reject']}`}
-            onClick={() => setConsent(TRACKING_CONSENT.DENIED)}
+            onClick={() => handleConsent(TRACKING_CONSENT.DENIED)}
           >
             Reject
           </button>
