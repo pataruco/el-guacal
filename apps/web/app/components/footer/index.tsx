86ef5862b351d216b17445682eff7bd9e69bf71c
Pedro Martin <pataruco@gmail.com>
2026-04-13 13:49:04 +0200

    chore: add privacy policy to footer

--- apps/web/app/components/footer/index.tsx
+++ apps/web/app/components/footer/index.tsx
@@ -1,44 +1,20 @@
+import { Link, useParams } from 'react-router';
+import type { ContentLocale } from '@/i18n';
 import styles from './index.module.scss';
 
-// const Footer = () => {
-//   return (
-//     <footer className={styles['c-footer']}>
-//       <div className={styles['c-footer__container']}>
-//         <p>
-//           El Guacal © {new Date().getFullYear()} by{' '}
-//           <a href="https://pataruco.dev">Pedro Martín Valera </a>
-//         </p>
-//         <p>
-//           <a href="https://creativecommons.org/licenses/by/4.0/">
-//             Licenced Creative Commons Attribution 4.0 International
-//           </a>
-//         </p>
-//         <div className={styles['c-footer__cc']}>
-//           <picture>
-//             <img
-//               src="https://mirrors.creativecommons.org/presskit/icons/cc.svg"
-//               alt="Creative Commons"
-//             />
-//           </picture>
-//           <picture>
-//             <img
-//               src="https://mirrors.creativecommons.org/presskit/icons/by.svg"
-//               alt="Creative Commons By"
-//             />
-//           </picture>
-//         </div>
-//       </div>
-//     </footer>
-//   );
-// };
 const Footer = () => {
+  const { locale } = useParams<{ locale: string }>();
+  const currentLocale = (locale as ContentLocale) || 'en';
+
   return (
     <footer className={styles['c-footer']}>
       <div className={styles['c-footer__container']}>
         <nav>
           <ul>
             <li>
-              <a href="/privacy-policy">Privacy policy</a>
+              <Link to={`/${currentLocale}/privacy-policy`}>
+                Privacy policy
+              </Link>
             </li>
           </ul>
         </nav>
